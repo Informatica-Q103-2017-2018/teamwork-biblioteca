0f1800c02547178d733f5bfc38e800bb885a43d7
huguiigar <38069816+huguiigar@users.noreply.github.com>
2018-05-09 13:31:31 +0200

    parte 1

---
+++ primeras cosas.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#define N 11
+float medialibro(int puntuacion,int numero);
+typedef struct
+{
+	char nom[N];
+	char cont[N];
+}usuario;
+void main()
+{
+	FILE *usuarios;
+	char a;
+	miembros = fopen("alumnos.txt", "r")
+	printf("                         BIENVENIDO A LA BIBLIOTECA-Q103                   \n");
+	printf("¿Esta usted registrad@ en esta biblioteca?\n");
+	printf("Escriba '1' si esta registrado o '2' si no lo esta\n ");
+	scanf("%i",&a);
+	switch(a)
+	{
+		case '1':
+		{
+			printf("Puede continuar introduciendo el nombre y contraseña del usuario:\n ");
+			printf("Usuario:\n");
+			scanf("%s",&usuario.nom);
+			printf("Contraseña:\n");
+			scanf("%s",&usuario.cont);
+			for ()
+			if 
+			
+			
+			
+			
+		}
+		case '2':
+		{
+			printf("A continuacion debera registrarse en la biblioteca\n");
+			printf("Introduzca su nombre de usuario con un maximo de 10 espacios:");
+			scanf("%s",&usuario.nom);
+			printf("Introduzca su contraseña de usuario con un maximo de 10 espacios:");
+			scanf("%s",&usuario.cont);
+			fopen("alumnos.txt", "w")
+			if (pf == NULL) 
+			{ 
+				printf("Error al abrir el fichero.\n"); 
+				return -1; 
+			} 
+			else 
+			{ 
+				printf("Fichero abierto correctamente.\n"); 
+				return 0; 
+			}
+			fprintf(miembros)
+
+			
+		}
+	}
+	
+	printf("A continuacion debera registrarse en la biblioteca\n");
+}
